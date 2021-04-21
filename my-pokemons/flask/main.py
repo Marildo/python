616d26dbe37542ed7964459c893ba988c9d6def7
Marildo Cesar <marildocesar@hotmail.com>
2021-04-21 17:33:17 -0300

    Vue-router does not work with flask ^_^

--- my-pokemons/flask/main.py
+++ my-pokemons/flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect,url_for
 
 app = Flask('Flask_AND_VUE', static_folder='../vue/dist', template_folder='templates')
 
@@ -7,6 +7,12 @@ app = Flask('Flask_AND_VUE', static_folder='../vue/dist', template_folder='templ
 def index():
     # return jsonify(msg='Server is running', data=datetime.now())
     return render_template('index.html')
+
+
+@app.route('/js/about.js')
+def all():
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
--- my-pokemons/vue/src/App.vue
+++ my-pokemons/vue/src/App.vue
@@ -1,35 +1,21 @@
 <template>
-  <div>    
-    <MainLayout />
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello World Vue!" />
-  
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <button @click="test">Test</button>
   </div>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import MainLayout from './components/MainLayout.vue';
-
-
 export default {
-  name: "App",
-  components: {
-
-    HelloWorld,
-    MainLayout,
-  },
-
-  data() {
-    return {
-      pa: "/img/logo.png",
-    };
-  },
-
-  mounted() {},
-
-  methods: {},
-};
+  methods:{
+    test(){
+      console.log('testando')
+      this.$router.push('/about')
+    }
+  }
+}
 </script>
 
 <style>
@@ -37,6 +23,20 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
